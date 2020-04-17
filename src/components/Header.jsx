ae4e7bce7a2ccb221a0b3346b37d0a571faf7b15
Marat <markbass82@gmail.com>
2020-04-17 14:18:58 +0300

    созданы компоненты и добавлены фото

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header =()=> {
+    return (
+       <header className="header">
+            <img src="/image/logo.jpg" alt="/" className="logo"/>
+       </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Sidebar =()=> {
+    return (
+       <div className="sidebar">
+           <nav className="nav-menu">
+                <a href="#" className="menu-link">Home</a>
+                <a href="#" className="menu-link">About</a>
+                <a href="#" className="menu-link">Massage</a>
+                <a href="#" className="menu-link">Contact</a>
+                <a href="#" className="menu-link">Email</a>
+            </nav>
+       </div>
+    );
+}
+
+export default Sidebar;

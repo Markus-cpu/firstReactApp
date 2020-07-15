5bc6266a616acad36b82fd991c2a14fd6b9a497c
Marat <markbass82@gmail.com>
2020-07-15 08:24:20 +0300

    Добавил файлы

---
+++ src/components/Infoblock/Infoblock.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import c from './Infoblock.module.css';
+import Infoperson from './Infoperson/Infoperson';
+import Myposts from './Myposts/Myposts';
+
+
+
+
+const Infoblock = (props) => {
+    let mypostElement = props.mypostData.map(mypost => <Myposts massage={mypost.post} id={mypost.id}/>);
+    let newPostElement = props.myNewPost;
+
+    let onAddPost =()=> {
+        props.addPost();
+    };
+
+    let onPostChange =(e)=> {
+        let text = e.target.value;
+        props.updateNewPost(text);
+    };
+
+    return (
+        <div>
+            <img className={c.img} src="https://www.goldmansachs.com/worldwide/banner-img-1200x200.jpg" alt="" />
+            <Infoperson  profile={props.profile}/>
+            <div className={c.inputpost}>
+                 <textarea onChange={ onPostChange }  value={newPostElement}
+                           className={c.textarea}  placeholder="Your message here...."/>
+                 <button onClick={ onAddPost } className={c.button}>Send</button>
+                 {/*Когда происходит клик по кнопке, она вызывает функцию onAddPost, которую мы определили локально*/}
+            </div>
+            {mypostElement}
+        </div>
+    )
+};
+
+export default Infoblock;
---
+++ src/components/Infoblock/InfoblockContainer.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {addPost, setUserProfile, updateNewPost} from '../../Redux/contentPage-reducer';
+import {connect} from "react-redux";
+import * as axios from "axios";
+import Infoblock from "./Infoblock";
+import {withRouter} from "react-router-dom";
+
+//данная контейнерная компонента является оберткой для обычной компоненты (Infoblock)
+//сюда приходят данные и методы из store/state
+//для того чтобы оставить нашу обычную компоненту чистой, без всякой лишней информации из
+//мира store(BLL)
+/*const InfoblockContainer = (props) => {
+    let state = props.store.getState();
+    let addPost =()=> {
+        let action = addPostActionCreator();//функция actionCreator(для создания action object
+        props.store.dispatch(action);
+    };
+    let PostChange =(text)=> {
+        let action = updateNewPostActionCreator(text);
+        props.store.dispatch(action);
+    };
+    return (
+        <div>
+          <Infoblock addPost={addPost} updateNewPostChange={PostChange} mypostData={state.contentPage.mypostData}
+                     myNewPost={state.contentPage.myNewPost} dialogsData={state.messagesPage.dialogsData}/>
+        </div>
+    )
+};*/
+class InfoblockContainer extends React.Component {
+    componentDidMount() {
+        let userId = this.props.match.params.userId;
+        if(!userId) {
+            return userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`, {
+            withCredentials: true
+        })
+            .then(response => {
+                this.props.setUserProfile(response.data);
+            });
+    }
+    render =()=> {
+        return (
+            <div>
+                <Infoblock {...this.props} profile={this.props.profile} />
+            </div>
+        );
+    }
+}
+const mapStateToProps =(state)=> {
+    return {
+        mypostData: state.contentPage.mypostData,
+        myNewPost: state.contentPage.myNewPost,
+        profile: state.contentPage.profile
+    }
+};
+//получить еще и данные о маршруте, т.е URL
+//и далее эту новую компоненту передать в connect
+
+//контейнерная компонента создается connect'ом
+export default connect(mapStateToProps, {addPost, updateNewPost, setUserProfile})(withRouter(InfoblockContainer));

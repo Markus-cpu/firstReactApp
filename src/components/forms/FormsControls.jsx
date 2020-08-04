5ac0457dbbccf7a858b67f1507b6476535b72ffd
Marat <markbass82@gmail.com>
2020-08-04 16:53:15 +0300

    'Validaition forms'

--- src/components/forms/FormsControls.jsx
+++ src/components/forms/FormsControls.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import s from './FormsControls.module.css'
 
-export const Textarea =({input, meta, ...props})=> {
-    return (
-        <>
-            <textarea {...input} {...props} />
-        </>
 
+const FormControl =({input, meta, child, ...props})=> {
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={s.formControls + " " + (hasError ? s.error : "")}>
+            {props.children}
+            {hasError && <div><span>{meta.error}</span></div>}
+        </div>
     )
+}
+
+export const Textarea =(props)=> {
+    const {input, meta, child, ...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
+}
+export const Input =(props)=> {
+    const {input, meta, child, ...restProps} = props
+    return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
 }
